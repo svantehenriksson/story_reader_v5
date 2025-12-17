51c23fd467da2f32be1a6f8b3d7ccf5eacbe660d
Svante Henriksson <svante.henriksson@gmail.com>
2025-12-17 06:31:06 +0200

    update

--- super_simple_version_text.js
+++ super_simple_version_text.js
@@ -1,5 +1,351 @@
-coconut_story.js
+const storyFinnish = `
+Luca on lentokentä -llä. 
+Luca on väsynyt. 
+Luca juo kahvi -a. 
+Kahvi on kallis -ta. 
+Kahvi on vähän outo -a. 
+Mutta kahvi auttaa. 
+Suomalai -set juo -vat tätä joka päivä? 
+Luca ets -ii bussi -a. 
+Bussi tul -ee. 
+Kuljettaja ei puhu englanti -a. 
+Kuljettaja ei puhu mitään. 
+Luca sanoo: "Hei." 
+Kuljettaja ei sano mitään. 
+Ulkona sataa lun -ta. 
+Paljon lun -ta. 
+On kylmä. 
+Tosi kylmä. 
+Lumi on kaunis -ta. 
+Luca hymyil -ee. 
+Tämä on Suomi.`;
 
+const storyFakeEnglish = `
+Luca is airport -at. 
+Luca is tired. 
+Luca drinks coffee -some. 
+Coffee is expensive -some. 
+Coffee is a_bit strange -some. 
+But coffee helps. 
+Finn -s(the) drink -they this every day? 
+Luca look -he bus -some. 
+The_bus come -s. 
+The_driver not speak English -some. 
+The_driver not say anything. 
+Luca says: "Hi." 
+The_driver not say anything. 
+Outside rains snow -some. 
+A_lot_of snow -some. 
+It's cold. 
+Really cold. 
+The_snow is beautiful -some. 
+Luca smile -s. 
+This is Finland.
+`;
+
+const storyEnglish = `
+Luca is at the airport. 
+Luca is tired. 
+Luca drinks coffee. 
+The coffee is expensive. 
+The coffee is a bit strange.    
+But the coffee helps.    
+Finns drink this every day?    
+Luca is looking for the bus.    
+The bus arrives.    
+The driver doesn't speak English.    
+The driver doesn't say anything.    
+Luca says: "Hi."    
+The driver doesn't say anything.    
+It's snowing outside.    
+A lot of snow.    
+It's cold.    
+Really cold.    
+The snow is beautiful.    
+Luca smiles.    
+This is Finland.`;
+
+
+
+const story1SpokenFinnish = `
+Luca on lentokentä-l.    
+Luca on väsyny.    
+Luca juo kahvi-i.    
+Kahvi on kallis-t.    
+Kahvi on vähän outo-o.    
+Mut kahvi auttaa.    
+Suomalai-set juo- tätä joka päivä?"    
+Luca ett-ii bussi-i.    
+Bussi tul-ee.    
+Kuljettaja ei puhu englanti-i.    
+Kuljettaja ei puhu mitää.    
+Luca sanoo: "Hei."    
+Kuljettaja ei sano mitää.    
+Ulkona sataa lun-ta.    
+Paljon lun-ta.    
+On kylmä.    
+Tosi kylmä.    
+Lumi on kaunis-t.    
+Luca hymyil-ee.    
+Tää on Suomi.`;
+
+const story2Finnish = `
+Luca menee hissi-in.    
+Hissi-ssä on nainen.    
+Nainen seiso-o hiljaa.    
+Luca seiso-o myös hiljaa.    
+Luca katso-o nais-ta.    
+Nainen katso-o Luca-a.    
+Luca sano-o: "Hei."    
+Nainen sano-o: "Moi."    
+Nainen hymyil-ee.    
+Luca hymyil-ee myös.    
+Luca kysy-y: "Mikä sinu-n nime-si on?"    
+Nainen sano-o: "Aino."    
+Luca: "Minu-n nimi on Luca."    
+Luca: "Asu-n toise-ssa kerrokse-ssa."    
+Aino: "Asu-n yläkerra-ssa-si."    
+Hissi pysähty-y.    
+Ding!    
+Luca astu-u ulos.`;
+
+const story2English = `
+Luca goes into the elevator.    
+There is a woman in the elevator.    
+The woman stands silently.    
+Luca also stands silently.    
+Luca looks at the woman.    
+The woman looks at Luca.    
+Luca says: "Hello."    
+The woman says: "Hi."    
+The woman smiles.    
+Luca smiles as well.    
+Luca asks: "What is your name?"    
+The woman says: "Aino."    
+Luca: "My name is Luca."    
+Luca: "I live on the second floor."    
+Aino: "I live upstairs from you."    
+The elevator stops.    
+Ding!    
+Luca steps out.`;
+
+const story2SpokenFinnish = `
+Luca menee hissi-i.    
+Hissi-ssä on nainen.    
+Nainen seiso-o hiljaa.    
+Luca seiso-o myös hiljaa.    
+Luca katso-o nais-ta.    
+Nainen katso-o Luca-a.    
+Luca sano-o: "Hei."    
+Nainen sano-o: "Moi."    
+Nainen hymyil-ee.    
+Luca hymyil-ee myös.    
+Luca kysy-y: "Mikä su-n nime-s on?"    
+Nainen sano-o: "Aino."    
+Luca: "Mu-n nimi on Luca."    
+Luca: "Asu-n toka-s kerrokse-s."    
+Aino: "Asu-n yläkerra-ssa-s."    
+Hissi pysähty-y.    
+Ding!    
+Luca astu-u ulos.`;
+
+const story3Finnish = `
+Luca-lla on kylmä.   
+Luca menee vaatekauppa-an.   
+Myyjä: Hei! Miten voi-n auttaa?   
+Luca: Minu-lla on kylmä.   
+Myyjä: Suosittel-en pitk-iä kalsar-eita.   
+Luca: Suosittel-et pitk-iä kalsar-eita?   
+Myyjä: Kyllä. Kuten kalsarit, mutta pitkä-t. Ne näyttä-vät leggings-eiltä.   
+Luca: Mies-ten leggingsit.   
+Myyjä: Ja sinu-n takki on ohut. Osta talvitakki.   
+Luca: Missä talvitaki-t ovat?   
+Myyjä: Suoraan eteenpäin ja vasemma-lle.   
+Luca: Kiitos.   
+Luca löytää musta-n talvitaki-n.   
+Myyjä: Siinä kaikki?   
+Luca: Siinä kaikki.   
+Myyjä: Osta vielä pipo.   
+Luca: Okei, pipo vielä.   
+Myyjä: Musta vai valkoinen?   
+Luca: Valkoinen.   
+Myyjä: 238 euro-a, kiitos.   
+Luca maksaa korti-lla.   
+Luca: Kiitos! Hei hei.   
+Myyjä: Kiitos! Moi moi.   
+`;
+
+const story3English = `
+Luca is cold.   
+Luca goes to a clothing store.   
+Salesperson: Hi! How can I help?   
+Luca: I am cold.   
+Salesperson: I recommend longjohns.   
+Luca: You recommend longjohns?   
+Salesperson: Yes. Like underwear, but long. They look like leggings.   
+Luca: Men's leggings.   
+Salesperson: And your jacket is thin. Buy a winter coat.   
+Luca: Where are the winter coats?   
+Salesperson: Straight ahead and to the left.   
+Luca: Thanks.   
+Luca finds a black winter coat.   
+Salesperson: Is that all?   
+Luca: That’s all.   
+Salesperson: Buy a beanie too.   
+Luca: Okay, a beanie too.   
+Salesperson: Black or white?   
+Luca: White.   
+Salesperson: 238 euros, please.   
+Luca pays by card.   
+Luca: Thanks! Bye bye.   
+Salesperson: Thanks! Bye bye.   
+`;
+
+const story3SpokenFinnish = `
+Luca-l on kylmä.   
+Luca menee vaatekauppa-a.   
+Myyjä: Moi! Mite voin auttaa?   
+Luca: Mu-lla on kylmä.   
+Myyjä: Suosittel-en pitk-ii kalsar-eit.   
+Luca: Suosittel-et pitk-ii kalsar-eit?   
+Myyjä: Joo. Niinku kalsarit mut pitkät. Ne näyttää leggingseilt.   
+Luca: Mies-ten leggingsit.   
+Myyjä: Ja su-n takki on ohut. Osta talvitakki.   
+Luca: Mis talvitak-it on?   
+Myyjä: Suoraa eteenpäin ja vasemma-l.   
+Luca: Kiitos.   
+Luca löytää musta-n talvitaki-n.   
+Myyjä: Siinä kaikki?   
+Luca: Siinä kaikki.   
+Myyjä: Osta viel pipo.   
+Luca: Okei, pipo viel.   
+Myyjä: Musta vai valkonen?   
+Luca: Valkonen.   
+Myyjä: 238 euro-o, kiitti.   
+Luca maksaa korti-l.   
+Luca: Kiitos! Hei hei.   
+Myyjä: Kiitos! Moi moi.   
+`;
+
+const story4Finnish = `
+Pomo: Tervetuloa!   
+Luca: Kiitos.   
+Pomo ja Luca kättele-vät.   
+Pomo: Minä ole-n Jarkko.   
+Luca: Minä ole-n Luca.   
+Jarkko: Halua-t-ko kahvi-a?   
+Luca: Kyllä, kiitos.   
+Jarkko: Minä-kin ota-n kahvi-a. Maanantaiaamu.   
+Mie-het otta-vat kahvi-a automaati-sta.   
+Automaati-sta lähte-e kova ääni: surrrr.   
+Luca: Täällä on tosi vähän ihmi-siä.   
+Jarkko: Hybridityö.   
+Luca: Ahaa.   
+Jarkko: Minu-lla on ensimmäinen työ sinu-lle.   
+Luca: Mikä työ?   
+Jarkko: Tekoälyprojekti. Asiakkaa-lle Ruoholahde-ssa.   
+Luca: Miten pääse-n Ruoholaht-een?   
+Jarkko: Ota raitiovaunu. Tai yrityk-sen sähköpyörä. Siinä on talvirenka-at.   
+`;
+
+const story4English = `
+Boss: Welcome!   
+Luca: Thanks.   
+The boss and Luca shake hands.   
+Boss: I'm Jarkko.   
+Luca: I'm Luca.   
+Jarkko: Do you want coffee?   
+Luca: Yes, please.   
+Jarkko: I will have coffee too. Monday morning.   
+The men take coffee from the machine.   
+The machine makes a loud sound: brrrr.   
+Luca: There are very few people here.   
+Jarkko: Hybrid work.   
+Luca: Aha.   
+Jarkko: I have your first task.   
+Luca: What task?   
+Jarkko: An artificial intelligence project. For a client in Ruoholahti.   
+Luca: How do I get to Ruoholahti?   
+Jarkko: Take the tram. Or the company's electric bike. It has winter tires.   
+`;
+
+const story4SpokenFinnish = `
+Pomo: Tervetuloo!   
+Luca: Kiitos.   
+Pomo ja Luca kättel-ee.   
+Pomo: Mä oo-n Jarkko.   
+Luca: Mä oo-n Luca.   
+Jarkko: Halu-u-ks kahvi-i?   
+Luca: Joo, kiitos.   
+Jarkko: Mä-kin ota-n kahvi-i. Maanantaiaamu.   
+Miehe-t otta-a kahvi-i automaati-st.   
+Automaati-st lähte-e kova ääni: surrrr.   
+Luca: Tääl on tosi vähän ihmi-sii.   
+Jarkko: Hybridityö.   
+Luca: Ahaa.   
+Jarkko: Mu-l on eka duuni su-lle.   
+Luca: Mikä duuni?   
+Jarkko: Tekoälyprojekti. Asiakkaa-l Ruoholahde-ssa.   
+Luca: Miten pääse-n Ruoholaht-ee?   
+Jarkko: Ota ratikka. Tai firma-n sähköpyörä. Siin on talvirenka-at.   
+`;
+
+const story5Finnish = `
+Töi-den jälkeen Luca on yksin kotona.   
+Luca on keittiö-ssä.   
+Luca katso-o kaappi-in.   
+Kaapi-ssa on tee-tä ja kahvi-a.   
+Kaapi-ssa on myös näkkileipä-ä.   
+Luca katso-o jääkaappi-in.   
+Jääkaapi-ssa on maito-a ja voi-ta.   
+Maito on vanha-a.   
+Onneksi on maitojauhetta   
+Luca keittää kupi-n tee-tä.   
+Luca laittaa voi-ta näkkileiväl-le.   
+Näkkileipä maistu-u vähän oudo-lta.   
+Lämmin tee maistu-u hyvä-ltä.   
+Luca selaa sosiaalis-ta media-a.   
+Tee-n jälkeen Luca katso-o elokuva-a.   
+Elokuva-n jälkeen Luca men-ee nukku-maan.   
+`;
+
+const story5English = `
+After work, Luca is home alone.   
+Luca is in the kitchen.   
+Luca looks in the cupboard.   
+There is tea and coffee in the cupboard.   
+There is also crispbread in the cupboard.   
+Luca looks in the fridge.   
+There is milk and butter in the fridge.   
+The milk is old.   
+Luckily there is milk powder.   
+Luca makes a cup of tea.   
+Luca puts butter on the crispbread.   
+The crispbread tastes a bit strange.   
+The warm tea tastes good.   
+Luca scrolls through social media.   
+After the tea, Luca watches a movie.   
+After the movie, Luca goes to sleep.   
+`;
+
+const story5SpokenFinnish = `
+Töide-n jälkeen Luca on yksin kotona.   
+Luca on keittiö-s.   
+Luca katto-o kaappi-in.   
+Kaapi-s on tee-tä ja kahvi-i.   
+Kaapi-s on kans näkkileipä-ä.   
+Luca katto-o jääkaappi-in.   
+Jääkaapi-s on maito-o ja voi-ta.   
+Maito on vanha-a.   
+Onneks on maitojauhetta.   
+Luca keittää kupi-n tee-tä.   
+Luca laittaa voi-ta näkkileivä-l.   
+Näkkileipä maistu-u vähä oudo-lt.   
+Lämmin tee maistu-u hyvä-lt.   
+Luca selaa somee.   
+Tee-n jälkeen Luca katt-oo leffa-a.   
+Leffa-n jälkeen Luca mene-e nukku-u.   
+`;
 /*
 const storyFinnish = `
 Eilen ennuste oli lämmin ja aurinko -inen. 
